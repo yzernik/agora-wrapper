5fadfee2362aea35254895527d7ea4478f7f978d
Mariusz Kogen <k0gen@pm.me>
2022-06-14 18:24:08 +0200

    Making config user friendly

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,6 +6,8 @@ AGORA_PORT=8080
 LND_RPC_AUTHORITY="lnd.embassy:10009"
 TLS_CERT_PATH="/mnt/lnd/tls.cert"
 INVOICES_MACAROON_PATH="/mnt/lnd/data/chain/bitcoin/mainnet/invoice.macaroon"
+PAYMENT=$(yq e '.payments' /root/start9/config.yaml)
+PRICE=$(yq e '.price' /root/start9/config.yaml)
 
 # Create directory for the agora files
 mkdir -p /mnt/filebrowser/${FILES_DIR}
@@ -18,6 +20,8 @@ paid: true
 # price for files in satoshis
 base-price: 500 sat
 AGR
+sed -i "s/paid:.*/paid: ${PAYMENT}/g" /mnt/filebrowser/Agora-Test/Satoshi/.agora.yaml
+sed -i "s/base-price:.*/base-price: ${PRICE} sat/g" /mnt/filebrowser/Agora-Test/Satoshi/.agora.yaml
 echo "Agora config file created"
 fi
 
