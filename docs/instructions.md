dba81f1102ad061bd7188af656078849e3880c76
Matt Hill <matthewonthemoon@gmail.com>
2022-10-07 09:39:51 -0600

    update copy

--- docs/instructions.md
+++ docs/instructions.md
@@ -1,10 +1,19 @@
-# Instructions for Agora
+# Explanation
 
-## Agora First Steps
-1 - Click on **"Config"** and configure Agora settings.
+Upon starting up, Agora will receive a unique directory inside File Browser. Any file you place inside this directory will be displayed in Agora.
 
-2 - Open `Filebrowser` and add files to the Agora files directory.
+You can let people view your files for free, or require them to pay you in Bitcoin over the Lightning Network to download and view your files.
 
-3 - Click on **"Interfaces"** and copy the Tor address to share it publicly.
+# Getting Started
 
-**Note:** You can configure a sell price per subdirectory by adding a custom `.agora.yaml` config file to it. You can learn more about access configuration [here](https://github.com/agora-org/agora#access-configuration).
+## Config
+
+Decide which folder in File Browser will be used, choose your preferred lightning implementation (LND or CLN), and set your default price for file downloads. Your chosen directory is automatically created inside File Browser, along with a sample file for testing.
+
+## Adding new files
+
+Open `File Browser` and add files to your Agora directory. You can create sub directories as well. By creating sub directories, you can customize the appearance and price of files inside that directory. Learn more about access configuration [here](https://github.com/agora-org/agora#access-configuration).
+
+## Sharing your files
+
+Simply share your Agora Tor address (found in **"Interfaces"**). Do not share your Agora LAN address, as that is only accessible to someone who is connected to you LAN.
--- scripts/services/getConfig.ts
+++ scripts/services/getConfig.ts
@@ -11,13 +11,14 @@ export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
      "interface": "main"
   },
   "directory": {
-     "name": "Directory Path",
-     "description": "The path to the directory in File Browser that will be served on your Agora .onion address. For example, a value of \"satoshi-files\" would tell Agora to look for that directory or create it in File Browser.",
+     "name": "Root Directory Path",
+     "description": "The path to the root directory in File Browser that will host your Agora files. If this directory does not already exist, it will be created upon saving this config.",
      "type": "string",
      "pattern": "^(\\.|[a-zA-Z0-9_ -][a-zA-Z0-9_ .-]*|([a-zA-Z0-9_ .-][a-zA-Z0-9_ -]+\\.*)+)(/[a-zA-Z0-9_ -][a-zA-Z0-9_ .-]*|/([a-zA-Z0-9_ .-][a-zA-Z0-9_ -]+\\.*)+)*/?$",
      "pattern-description": "Must be a valid relative file path",
      "nullable": false,
-     "default": "agora-files"
+     "default": "agora-files",
+     "placeholder": "e.g. 'agora-files', 'bitcoin-stuff/agora', etc"
   },
   "lightning": {
    "type": "object",
@@ -34,25 +35,25 @@ export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
            "cln",
          ],
          "value-names": {
-           "lnd": "Lightning Network Daemon",
-           "cln": "Core Lightning",
+           "lnd": "Lightning Network Daemon (LND)",
+           "cln": "Core Lightning (CLN)",
          },
          "default": "lnd",
        },
        "payments": {
          "type": "boolean",
          "name": "Enable payments",
-         "description": "Decide whether to charge for files via Lightning or make them freely available.",
+         "description": "Decide whether to charge for files or make them freely available. This can be overridden on a subdirectory basis.",
          "default": true,
       },
        "price": {
          "type": "number",
-         "name": "Price for files in satoshis",
+         "name": "Price for files (in satoshis)",
          "nullable": false,
          "range": "[1,10000000]",
          "integral": true,
          "units": "satoshis",
-         "description": "The amount of satoshis users will pay per each file you host on Agora.",
+         "description": "The default amount of satoshis users must pay per each file you host on Agora. This can be overridden on a subdirectory basis.",
          "default": 500
        },
        },
